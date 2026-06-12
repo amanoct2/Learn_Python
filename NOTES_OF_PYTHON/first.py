41d7a36936af32a4ec27f12d4ee0cbab90201bf8
Aman <ankitsharma11052005@gmail.com>
2026-06-12 05:10:14 +0000

    add funtions

--- NOTES_OF_PYTHON/first.py
+++ NOTES_OF_PYTHON/first.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print("Good Morning Sir")
+
+#function in python
+def function_HOD():
+    print("Hello Sir")
+    print("HBD Sir")
+    print("Good HOD Sir")
+    print("Good Sir")
+
+function_HOD()
+function_HOD()
+function_HOD()
